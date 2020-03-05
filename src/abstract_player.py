d2c4870849554176634177ad0c218d215e2c67c8
patrik999 <patrik999@gmail.com>
2020-03-05 17:08:12 +0100

    added configuration

---
+++ src/abstract_player.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import getopt, sys
+import optparse
+import time
+
+
+# Base class for a reader
+class AbstractPlayer:  #__metaclass__ = ABCMeta
+
+    streamID = ""
+    templateID = ""
+    frequency = 0.1 # Update every 100ms
+
+
+    def __init__(self, stream_id, template_id):  #
+        self.streamID = stream_id
+        self.templateID = template_id
+
+
+    def start(self, freq_in_ms):
+        # Here comes the open. This does not need be be overriten
+        return
+
+    def stop(self):
+        # Here comes the read and yield
+        #raise NotImplementedError
+        return
+
+    def modify(self, freq_in_ms):
+        return
+        #def add_noise(self, freq_in_ms):
+
+
+    def close(self):
+        # Here comes the close. This does not need be overriten
+        return
---
+++ src/vissim_player.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import getopt, sys
+import optparse
+import time
+import abstract_player # Import file
+
+
+class VissimPlayer(AbstractPlayer):
+
+
+
+    def init(self, stream_id, template_id):  # __init__
+        self.streamID = stream_id
+        self.templateID = template_id
+
+
+    def start(self, freq_in_ms):
+
+        self.frequency = freq_in_ms
+
+        for index in range(self.rounds):
+            #print self.constantTexts[index]
+            msgText = "Test Msg " + str(index)
+            yield msgText
+            time.sleep(self.frequency)
+
+    def modify(self, freq_in_ms):
+        self.frequency = freq_in_ms
+
+
+def main(argv):
+
+    player = VissimPlayer("SID", "TID")
+
+    print("Start streaming...")
+
+    for msg in player.start(0): # 0.1
+        print(msg)
+
+    print("Stop streaming.")
+
+    player.close()
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1:])
