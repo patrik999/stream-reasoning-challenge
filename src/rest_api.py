fe503b493374ae2a15c8607b1734fc469511f27d
Patrik Schneider <patrik999@gmail.com>
2021-08-27 12:03:47 +0200

    Extenstion of rest_api.py initialization
    Allows to read more templates according to templates/templateType, instead of a single one

--- src/rest_api.py
+++ src/rest_api.py
@@ -6,36 +6,47 @@ import threading
 
 class RestApi():
     config=None
-    
+
     def __init__(self, config):
         self.config=config
         #init rest api
         self.api = Flask(__name__)
-        
+
         #init websocket server
         self.wsServer = WSServer(port=self.config["websocketserver"]["port"], host=self.config["websocketserver"]["host"])
-        
+
         #=== define api routes ===
         @self.api.route('/init', methods=['GET'])
         def init_route():#usage: /init?streamtype=vissim&streamid=streamVissim1&templatetype=traffic-json&templateid=substreamVissim1
             #input parameters
-            streamType=request.args.get('streamtype', default = "vissim", type = str)
-            streamID=request.args.get('streamid', default = "streamVissim1", type = str)            
+            streamType=request.args.get('streamtype', default = "sumo", type = str) # vissim
+            streamID=request.args.get('streamid', default = "streamSumo1", type = str) # streamVissim1
             templateType=request.args.get('templatetype', default = "traffic-json", type = str)
-            templateID=request.args.get('templateid', default = "substreamVissim1", type = str)
-            
+            # PS: Changed logic for template ids, if no templated id is given load all templates of type and add them to a dictionary
+            #templateID=request.args.get('templateid', default = "substreamVissim1", type = str)
+
             #check if type match with id
             if streamID not in self.config["streams"][streamType]:
-                return json.dumps({"error":"stream ID does not match with stream type"}), 400
-            if templateID not in self.config["templates"][templateType]:
-                return json.dumps({"error":"template ID does not match with template type"}), 400
-            
+                return json.dumps({"error":"stream ID does not match with /streams/ids."}), 400
+
+            # Read all ids and path from streamType in dictionary, this will be give to the player
+            templates = {}
+            if templateType not in self.config["templates"][templateType]:
+                docs = self.config["templates"][templateType]
+                for key, val in docs.items():
+                    #print(key, ":", val)
+                    templates[key] = val
+            else:
+                return json.dumps({"error":"template type does not match with /templates/types."}), 400
+            #if templateID not in self.config["templates"][templateType]:
+            #    return json.dumps({"error":"template ID does not match with template type"}), 400
+
             #load player
             PlayerClass=self.player_class_loader(RELATIVE_ROOT_PATH+self.config["player"][streamType]["path"], self.config["player"][streamType]["class"])
-            
+
             #init player
-            self.player=PlayerClass(self.config["streams"][streamType][streamID], self.config["templates"][templateType][templateID])
-            
+            self.player=PlayerClass(self.config["streams"][streamType][streamID], templates) # self.config["templates"][templateType][templateID]
+
             return json.dumps({"websocket_url":"ws://"+self.config["websocketserver"]["host"]+":"+str(self.config["websocketserver"]["port"])}), 200
 
         @self.api.route('/getkb', methods=['GET'])
@@ -46,15 +57,15 @@ class RestApi():
         def start_route(): #usage: /start?frequency=10
             #input parameters
             frequency=request.args.get('frequency', default = 10, type = int)
-            
+
             #start broadcast messages from player (using multithreading)
             broadcast_thread=threading.Thread(target=self.broadcasting_thread, args=(self.player.start(frequency), ))
             broadcast_thread.start()
-            
+
             return json.dumps({"message":"success"}), 200
 
         @self.api.route('/stop', methods=['GET'])
-        def stop_route(): #usage: /stop    
+        def stop_route(): #usage: /stop
             self.player.stop()
             return json.dumps({"message":"success"}), 200
 
@@ -63,20 +74,18 @@ class RestApi():
             frequency=request.args.get('frequency', default = 10, type = int)
             self.player.modify(frequency)
             return json.dumps({"message":"success"}), 200
-    
+
     def player_class_loader(self, path, class_name):
         return run_path(path)[class_name]
-    
+
     def broadcasting_thread(self,messages):
         for msg in messages:
             self.wsServer.broadcast(msg)
-    
+
     def run(self):
         #start websocket server
         self.wsServer.run()
-        
+
         #start REST API server
         apiThread=threading.Thread(target=self.api.run, args=(self.config["restapi"]["host"], self.config["restapi"]["port"], ))
         apiThread.start()
-            
-    
