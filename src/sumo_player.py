3f5a98688634967f51662a9112328ebdfcb6d5c0
Patrik Schneider <patrik999@gmail.com>
2021-09-24 17:17:51 +0200

    Fixed sleep() to ms

--- src/sumo_player.py
+++ src/sumo_player.py
@@ -1,6 +1,7 @@
-import os
-import sys
-import time
+#!/usr/bin/env python
+
+
+import os, sys, time
 import optparse
 import json
 import uuid
@@ -21,7 +22,7 @@ else:
 from sumolib import checkBinary  # noqa
 import traci  # noqa
 
-from abstract_player import AbstractPlayer  # Import file
+from abstract_player import AbstractPlayer # Import file
 
 
 class SumoPlayer(AbstractPlayer):
@@ -44,6 +45,7 @@ class SumoPlayer(AbstractPlayer):
     rows_pos = {}
     lane_mappings = {}
 
+
     def __init__(self, sumo_config, template_dict):  # __init__
         self.streamID = sumo_config
         #self.templateID = template_path
@@ -54,9 +56,19 @@ class SumoPlayer(AbstractPlayer):
         self.stopped = True
         print("SUMO is stopped")
 
+    def getkb(self):
+
+
+        if "backgroundKB" in self.templates:
+            kbName = self.templates["backgroundKB"]
+            kbText = open(kbName).read()
+            return kbText
+        else:
+            return ""
+
     def start(self, freq_in_ms):
 
-        self.frequency = freq_in_ms / 100  # Set to milliseconds * 10
+        self.frequency = freq_in_ms # / 100 # Set to milliseconds * 10
         self.stopped = False
         self.step = 0
         self.step2 = 0
@@ -70,9 +82,9 @@ class SumoPlayer(AbstractPlayer):
         tlCount = tlChange
 
         # Open template file
-        # print(str(self.templates))
+        #print(str(self.templates))
         tempVehName = self.templates["subStreamVehicles"]
-        # templ_file1 = open(tempVehName) #self.templateID
+        #templ_file1 = open(tempVehName) #self.templateID
         templ_vehicles = open(tempVehName).read()
 
         # Template has to be split into platfrom/sensor parts using $CHILDS$
@@ -92,24 +104,23 @@ class SumoPlayer(AbstractPlayer):
         self.loadTLPositions(lines)
 
         # Open Sumo config file
-        # config_file = open(self.streamID) # config_file
+        #config_file = open(self.streamID) # config_file
         #config_str = config_file.read()
         self.config_name = self.streamID
 
-        # if options.nogui: #not
+        #if options.nogui: #not
         sumoBinary = checkBinary('sumo')
-        # else:
+        #else:
         #    sumoBinary = checkBinary('sumo-gui')
 
-        print("SUMO is starting with ratio: " +
-              str(self.step_ratio) + " and " + self.config_name)
+        print("SUMO is starting with ratio: " + str(self.step_ratio) + " and " + self.config_name)
         print("Limit steps: " + str(self.steps_limit))
 
         #global step, step2, plan_changes, plan_received
 
         # this is the normal way of using traci. sumo is started as a
         # subprocess and then the python script connects and runs
-        sumoCmd = [sumoBinary, "-S", "-Q", "-c", self.config_name]
+        sumoCmd = [sumoBinary, "-S", "-Q", "-c", self.config_name ]
 
         #print("D1: " + str(sumoCmd))
 
@@ -126,7 +137,7 @@ class SumoPlayer(AbstractPlayer):
             traci.simulationStep()
 
             self.step = self.step + 1
-            time.sleep(self.frequency)  # sleep
+            time.sleep(self.frequency / 1000) # sleep
 
             if(self.steps_limit < self.step):
                 break
@@ -139,21 +150,20 @@ class SumoPlayer(AbstractPlayer):
             print("Step: " + str(self.step) + " / " + str(self.step2))
 
             vehicle_id_list = traci.vehicle.getIDList()
-            print("Nr. of vehicles: " + str(len(vehicle_id_list)))
+            print("Nr. of vehicles: " +  str(len(vehicle_id_list)))
 
             # Vehicles
-            vehicles_ids = traci.vehicle.getIDList()
+            vehicles_ids=traci.vehicle.getIDList();
 
-            def lc(s): return s[:1].lower() + s[1:] if s else ''
+            lc = lambda s: s[:1].lower() + s[1:] if s else ''
 
             for veh_id in vehicles_ids:
-                # traci.vehicle.setSpeedMode(veh_id,0)
-                # traci.vehicle.setSpeed(veh_id,15) #sets the speed of vehicles to 15 (m/s)
+                #traci.vehicle.setSpeedMode(veh_id,0)
+                #traci.vehicle.setSpeed(veh_id,15) #sets the speed of vehicles to 15 (m/s)
 
                 replaceValues = {}
                 replaceValues["$VehicleID$"] = str(veh_id)
-                replaceValues["$ObsID$"] = "O_" + \
-                    str(veh_id) + "-" + str(self.step)
+                replaceValues["$ObsID$"] = "O_" + str(veh_id) + "-" + str(self.step)
                 replaceValues["$Timestamp$"] = str(self.step)
 
                 veh_speed = round(traci.vehicle.getSpeed(veh_id), 3)
@@ -171,9 +181,9 @@ class SumoPlayer(AbstractPlayer):
                 #veh_laneID = str(traci.vehicle.getLaneID(veh_id))
                 veh_laneID = str(traci.vehicle.getRoadID(veh_id))
 
+
                 if veh_laneID in self.lane_mappings:
-                    # (sumo id, asp id, asp orientation)
-                    laneTuple = self.lane_mappings[veh_laneID]
+                    laneTuple = self.lane_mappings[veh_laneID] # (sumo id, asp id, asp orientation)
                     replaceValues["$LaneID$"] = laneTuple[1]
                     replaceValues["$LaneOrient$"] = laneTuple[2]
                 else:
@@ -183,12 +193,11 @@ class SumoPlayer(AbstractPlayer):
 
                 # X,Y Position
                 #posXY = veh_pos.split(',')
-                replaceValues["$Position_X$"] = str(round(veh_pos[0], 5))
-                replaceValues["$Position_Y$"] = str(round(veh_pos[1], 5))
+                replaceValues["$Position_X$"] = str(round(veh_pos[0],5))
+                replaceValues["$Position_Y$"] = str(round(veh_pos[1],5))
 
                 veh_signals = traci.vehicle.getSignals(veh_id)
-                veh_lane = str(traci.vehicle.getLaneID(veh_id)) + ";" + \
-                    str(round(traci.vehicle.getLanePosition(veh_id), 4))
+                veh_lane = str(traci.vehicle.getLaneID(veh_id)) + ";" + str(round(traci.vehicle.getLanePosition(veh_id), 4))
 
                 #print("Speed ", veh_id, ": ", veh_speed, " m/s")
                 #print("EdgeID of veh ", veh_id, ": ", traci.vehicle.getRoadID(veh_id))
@@ -197,12 +206,12 @@ class SumoPlayer(AbstractPlayer):
                 # Speed
                 msg1 = templ_vehicles
                 for key, val in replaceValues.items():
-                    msg1 = msg1.replace(key, val)
+                    msg1 = msg1.replace(key,val)
                 # print(msg1)
                 yield msg1
 
                 #msg1a = "speed(" + str(self.step2) + "," + str(veh_id) + "," + str(veh_speed) + ") ."
-                # yield msg1a
+                #yield msg1a
 
             # Lanes
             lane_id_list = traci.lane.getIDList()
@@ -210,17 +219,18 @@ class SumoPlayer(AbstractPlayer):
             # Traffic lights
             tl_id_list = traci.trafficlight.getIDList()
 
-            print("Intersections with TLs: " + str(len(tl_id_list)))
+            print("Intersections with TLs: " +  str(len(tl_id_list)))
 
             # Group of traffic lights
             for intersID in tl_id_list:
 
-                msg2 = templ_TLs.replace("$IntersectionID$", str(intersID))
+                msg2 = templ_TLs.replace("$IntersectionID$",str(intersID))
                 msg2_Childs = ""
 
                 hasChildTemplate = False
                 if(self.TEMPL_PLACEHOLDER_CHILDS in msg2):
                     hasChildTemplate = True
+
 
                 replaceValues2 = {}
                 tlIDcheck = {}
@@ -230,19 +240,19 @@ class SumoPlayer(AbstractPlayer):
                 # Get signals for one intersection
                 tl_states = traci.trafficlight.getRedYellowGreenState(intersID)
                 #msg3 = "tlPhase(" + str(self.step2) + "," + str(intersID) + "," + tl_states + ")"
-                # print(msg3)
+                #print(msg3)
 
                 if(intersID in self.rows_pos):
                     row_pos_values = self.rows_pos[intersID]
 
                     # Find tlight for position
-                    # row_pos is at tuple (tlid positions)
-                    for rowPosDict in row_pos_values:
+                    for rowPosDict in row_pos_values: # row_pos is at tuple (tlid positions)
                         tlID = rowPosDict["lane"]
                         tlPositions = rowPosDict["pos"]
                         #print(str(tlID) + str(tlPositions))
 
-                        for i in range(0, len(tl_states)):  # each char is one signal state
+
+                        for i in range(0, len(tl_states)): # each char is one signal state
 
                             tlState = tl_states[i]
                             if str(i+1) in tlPositions:
@@ -257,7 +267,7 @@ class SumoPlayer(AbstractPlayer):
                                     msg2_Temp = templ_TLs_Child
 
                                     for key, val in replaceValues2.items():
-                                        msg2_Temp = msg2_Temp.replace(key, val)
+                                        msg2_Temp = msg2_Temp.replace(key,val)
 
                                     if(len(msg2_Childs) > 0):
                                         msg2_Childs = msg2_Childs + ","
@@ -265,52 +275,55 @@ class SumoPlayer(AbstractPlayer):
                                 else:
                                     msg3 = msg2
                                     for key, val in replaceValues2.items():
-                                        msg3 = msg3.replace(key, val)
+                                        msg3 = msg3.replace(key,val)
                                     yield msg3
 
                                 tlIDcheck[tlID] = ""
 
+
                 # Case with child template (e.g., RDF)
                 if(hasChildTemplate):
-                    msg2 = msg2.replace(
-                        self.TEMPL_PLACEHOLDER_CHILDS, str(msg2_Childs))
+                    msg2 = msg2.replace(self.TEMPL_PLACEHOLDER_CHILDS, str(msg2_Childs))
                     yield msg2
                 # Case with single template (e.g., ASP) is handled in loop
 
-            # for lane_id in lane_id_list:
+
+
+            #for lane_id in lane_id_list:
                 #lane_count = int(traci.lane.getLastStepVehicleNumber(lane_id))
 
-                # if(lane_count > 0):
+                #if(lane_count > 0):
 
                     # Merge lanes to road
                     #msg2 = "info(VehicleCount," + str(self.step2) + "," + str(lane_id) + "," + str(lane_count) + ")"
-                    # yield msg2
-                    # if(not ws is None):
-                    # ws.send(msg1)
+                    #yield msg2
+                    #if(not ws is None):
+                        #ws.send(msg1)
 
                     #waiting_count = int(traci.lane.getWaitingTime(lane_id))
 
-                    # if(waiting_count > 0):
-                    #msg3 = "info(WaitingTime," + str(self.step2) + "," + str(lane_id) + "," + str(waiting_count) + ")"
-                    # yield msg3
-                    # if(not ws is None):
-                    #    ws.send(msg2)
+                    #if(waiting_count > 0):
+                        #msg3 = "info(WaitingTime," + str(self.step2) + "," + str(lane_id) + "," + str(waiting_count) + ")"
+                        #yield msg3
+                        #if(not ws is None):
+                        #    ws.send(msg2)
 
         print("Simulation finished.")
 
-        traci.close(False)  # Immediate close
+        traci.close(False) # Immediate close
         sys.stdout.flush()
+
 
     def modify(self, freq_in_ms):
         self.frequency = freq_in_ms
 
-        print("Frequency set to: " + str(freq_in_ms))
+        print("Frequency set to: " + str(freq_in_ms)) + " ms"
 
-    def loadTLPositions(self, linesBasePlan):  # namePlan, nameChg factor,
+    def loadTLPositions(self, linesBasePlan): #  namePlan, nameChg factor,
 
-        for line1 in linesBasePlan:  # f_open1:
+        for line1 in  linesBasePlan: # f_open1:
 
-            line1 = line1.strip()
+            line1=line1.strip()
 
             if len(line1) == 0:
                 continue
@@ -321,7 +334,7 @@ class SumoPlayer(AbstractPlayer):
                 hlength = 4
                 predName = line1[0:hlength]
 
-                posHead2 = hlength + 1  # Include bracket
+                posHead2 = hlength + 1 # Include bracket
                 posEnd2 = len(line1) - 2
                 lineData2 = line1[posHead2:posEnd2]
 
@@ -336,7 +349,7 @@ class SumoPlayer(AbstractPlayer):
                     for x in posString.split(";"):
                         posDict[x] = ""
 
-                    row = {'lane': laneID,  'pos': posDict}  # 'inters': insID,
+                    row = {'lane': laneID,  'pos': posDict } # 'inters': insID,
 
                     if intersID in self.rows_pos:
                         self.rows_pos[intersID].append(row)
@@ -347,14 +360,14 @@ class SumoPlayer(AbstractPlayer):
 
                 if(predName == "lmap"):
 
-                    landIdSumo = lineDataSplit[0].strip()
-                    lineTuple = (landIdSumo, lineDataSplit[1].strip(
-                    ), lineDataSplit[2].strip())  # sumo_id, asp_id, asp_dir
+                        landIdSumo = lineDataSplit[0].strip()
+                        lineTuple = (landIdSumo, lineDataSplit[1].strip(), lineDataSplit[2].strip()) # sumo_id, asp_id, asp_dir
 
-                    if not landIdSumo in self.lane_mappings:
-                        self.lane_mappings[landIdSumo] = lineTuple
+                        if not landIdSumo in self.lane_mappings:
+                            self.lane_mappings[landIdSumo] = lineTuple
 
         print("D1: " + str(self.lane_mappings))
+
 
 
 #####
@@ -362,27 +375,25 @@ class SumoPlayer(AbstractPlayer):
 #####
 def main(argv):
 
+
     #parser = optparse.OptionParser()
     #parser.add_option('--nogui', action="store_true", dest="nogui", default=False, help="Run the commandline version of sumo")
 
     templates = {}
-    # json
-    templates["subStreamVehicles"] = "stream-templates/traffic/vehicle_template.asp"
-    # json
-    templates["subStreamTrafficLights"] = "stream-templates/traffic/traffic_light_template.asp"
+    templates["subStreamVehicles"] = "stream-templates/traffic/vehicle_template.asp" # json
+    templates["subStreamTrafficLights"] = "stream-templates/traffic/traffic_light_template.asp" # json
     # templates["subStreamTrafficLightsChilds"] = "../stream-templates/traffic/traffic_light_template2.json"
 
     player = SumoPlayer("stream-log-files/sumo/testASP_180.sumocfg", templates)
 
     print("Start streaming...")
 
-    for msg in player.start(0.1):  # 0.1 0.5
+    for msg in player.start(0.1): # 0.1 0.5
         print(msg)
 
     print("Stop streaming.")
 
     player.close()
-
 
 if __name__ == "__main__":
     import sys
