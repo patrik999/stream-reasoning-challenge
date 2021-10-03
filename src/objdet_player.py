5c88ee9ceb87e957f5c134a17fd94748e0f1b9b8
Manh Nguyen Duc <ducmanh93vietnam@gmail.com>
2021-10-03 23:46:40 +0200

    add sensor data

--- src/objdet_player.py
+++ src/objdet_player.py
@@ -12,8 +12,10 @@ class ObjectDetection(AbstractPlayer):
         super().__init__(stream_id, template_dictionary)
 
         # read the log file and convert to a list of observations
-        f = open(self.streamID)
-        detections = json.load(f)
+        od = open(self.streamID+".od")
+        ss = open(self.streamID+".sensor")
+        detections = json.load(od)
+        sensors = json.load(ss)
         observations = {}
 
         # {"image_id": "0000000058", "category_id": 7, "label": "truck","bbox": [175.09146241137856, 182.35644541288679, 48.0617183258659, 26.0792264812871], "score": "28.61"},
@@ -27,7 +29,11 @@ class ObjectDetection(AbstractPlayer):
             observation = observations.get(image_id)
             if observation is None:
                 observation = Observation(image_id)
-                observation.add(result)
+                observation.add(result)  # adding object detection result
+
+                #{"lat": "49.019702312103", "lon": "8.4435252928258", "alt": "114.12394714355", "roll": "-0.023792", "pitch": "0.012376", "vf": "17.078529727139", "ax": "-0.45926757222713", "ay": "-0.24090805193072"}
+                observation.set_sensor(sensors[image_id])  # adding sensor data
+
                 observations[image_id] = observation
             else:
                 observation.add(result)
@@ -41,10 +47,11 @@ class ObjectDetection(AbstractPlayer):
             graph = observation.get_graph(graph)
             #graph.serialize(destination='output.nt', format='n3')
             message = str(graph.serialize(format=self.templateID))
-            message = message.replace('\\n', '\n').replace('b\'', '').replace('\'', '')
+            message = message.replace('\\n', '\n').replace(
+                'b\'', '').replace('\'', '')
             print(message)
 
-            #yield message
+            # yield message
             time.sleep(self.frequency / 1000.0)
 
     def modify(self, freq_in_ms):
@@ -58,9 +65,13 @@ class Observation:
     def __init__(self, id):
         self.id = id
         self.results = []
+        self.sensor = {}
 
     def add(self, result):
         self.results.append(result)
+
+    def set_sensor(self, sensor):
+        self.sensor = sensor
 
     def get_graph(self, graph):
         SR_NAMESPACE = 'http://stream-reasoning-challenge.org'
@@ -91,23 +102,31 @@ class Observation:
         result_id = 0
         for result in self.results:
             # print(result)
-            label = result['label'].replace(' ','')
+            label = result['label'].replace(' ', '')
             bbox = result['bbox']
 
-            detection_result_iri = URIRef(SR_NAMESPACE + '/detection/' + self.id + '/' + str(result_id))
-            box_result_iri = URIRef(SR_NAMESPACE + '/box/' + self.id + '/' + str(result_id))
-            detected_object = URIRef(SR_NAMESPACE + '/detectedObject/' + label + '/')
+            detection_result_iri = URIRef(
+                SR_NAMESPACE + '/detection/' + self.id + '/' + str(result_id))
+            box_result_iri = URIRef(
+                SR_NAMESPACE + '/box/' + self.id + '/' + str(result_id))
+            detected_object = URIRef(
+                SR_NAMESPACE + '/detectedObject/' + label + '/')
 
             graph.add((observation_iri, SOSA.hasResult, detection_result_iri))
             graph.add((detection_result_iri, RDF.type, detection_class_iri))
-            graph.add((detection_result_iri, has_detected_object, detected_object))
+            graph.add(
+                (detection_result_iri, has_detected_object, detected_object))
             graph.add((detection_result_iri, has_box, box_result_iri))
             graph.add((detected_object, RDFS.label, Literal(label)))
 
-            graph.add((box_result_iri, box_x, Literal(bbox[0], datatype=XSD.float)))
-            graph.add((box_result_iri, box_y, Literal(bbox[1], datatype=XSD.float)))
-            graph.add((box_result_iri, box_width, Literal(bbox[2], datatype=XSD.float)))
-            graph.add((box_result_iri, box_height, Literal(bbox[3], datatype=XSD.float)))
+            graph.add((box_result_iri, box_x, Literal(
+                bbox[0], datatype=XSD.float)))
+            graph.add((box_result_iri, box_y, Literal(
+                bbox[1], datatype=XSD.float)))
+            graph.add((box_result_iri, box_width,
+                      Literal(bbox[2], datatype=XSD.float)))
+            graph.add((box_result_iri, box_height,
+                      Literal(bbox[3], datatype=XSD.float)))
 
             graph.add((detection_result_iri, is_detection_of, frame_iri))
             graph.add((box_result_iri, is_in, frame_iri))
@@ -117,7 +136,8 @@ class Observation:
 
 
 def main():
-    player = ObjectDetection('stream-log-files/kitti/yolov4-2011_09_26_drive_0015-1.json', 'n3')
+    player = ObjectDetection(
+        'stream-log-files/kitti/2011_09_26_drive_0015', 'n3')
     player.start(1)
 
 
